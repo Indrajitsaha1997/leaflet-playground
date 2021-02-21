c224a4739eb35245a95b0033d378e979af66555c
Indrajit7597 <indrasaha1997@gmail.com>
2021-02-21 20:32:15 +0530

    leaflet map

--- src/app/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -7,23 +7,23 @@ import { antPath } from 'leaflet-ant-path';
   styleUrls: ['./leaflet-map.component.css']
 })
 export class LeafletMapComponent implements OnInit {
-  map: Leaflet.Map;
-  propertyList = [];
+  name = 'Angular';
+  map:any;
 
   constructor() { }
 
   ngOnInit() {
-    this.map = Leaflet.map('map').setView([43.068661, 141.350755], 8);
+    this.map = Leaflet.map('map').setView([20.5937, 78.9629], 5);
     Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: 'Indrajit Saha'
     }).addTo(this.map);
     Leaflet.marker([0, 0]).bindPopup('<b>Hello!!</b>').addTo(this.map);
 
     // antPolyline = L.polyline.antPath(latlngs, options);
     // antPolyline.addTo(map);
   
-    antPath([[43.068661, 141.350755], [42.768651, 141.750955]], {color: '#FF0000', weight: 5, opacity: 0.6}).addTo(this.map);
-    antPath([[43.668661, 140.250755], [42.368651, 141.150955]], {color: '#0000FF', weight: 5, opacity: 0.6, reverse: true,}).addTo(this.map);
+    antPath([[22.9868, 87.8550], [25.0961, 85.3131]], {color: '#FF0000', weight: 5, opacity: 0.6, reverse: true}).addTo(this.map);
+    // antPath([[43.668661, 140.250755], [42.368651, 141.150955]], {color: '#0000FF', weight: 5, opacity: 0.6, reverse: true,}).addTo(this.map);
   }
 
 }
